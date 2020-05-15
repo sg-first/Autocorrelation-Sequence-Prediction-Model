6bf5d4143e3d0b5721d58d5e3ce7eb79530a6ee6
怎样 <1391725470@qq.com>
2020-05-15 11:54:50 +0800

    完善保存

---
+++ 一号网络.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from keras import layers
+from keras.layers.normalization import BatchNormalization
+from keras import Input
+from keras.models import Model
+from keras import callbacks
+from keras.optimizers import adam
+from keras import regularizers
+callback_list=[
+    callbacks.EarlyStopping(monitor="loss",patience=200),
+    callbacks.ModelCheckpoint(filepath="toy_v5_s.h5",monitor="loss",save_best_only=True),
+    callbacks.ReduceLROnPlateau(monitor="loss",factor=0.85,verbose=1,patience=12)
+]
+infetion_A=pd.read_csv("infection_A.csv",names=["城市","区域","日期","新增感染人数"])
+infetion_A=infetion_A[["日期","区域","新增感染人数"]]
+print(infetion_A)
+worst_area=infetion_A[infetion_A["区域"]==39].iloc[:,2:]
+print(worst_area)
+label=np.array(worst_area)
+print(label)
+train=np.arange(1,46)
+train=(train-np.mean(train))/np.std(train)
+print(train)
+data_input=Input(shape=(1,))
+x=layers.Dense(4,activation="relu")(data_input)
+x=layers.Dense(8,activation="relu")(x)
+x=layers.Dense(16,activation="relu")(x)
+x=layers.Dense(32,activation="relu")(x)
+x=layers.Dense(64,activation="relu")(x)
+x=layers.Dense(128,activation="relu")(x)
+x=layers.Dense(256,activation="relu")(x)
+x=layers.Dense(512,activation="relu")(x)
+x=layers.Dense(1024,activation="relu")(x)
+predict_inf=layers.Dense(1)(x)
+predict_inf_model=Model(data_input,predict_inf)
+predict_inf_model.compile(optimizer=adam(),loss="mse")
+predict_inf_model.fit(train,label,epochs=20000,batch_size=9,callbacks=callback_list)
