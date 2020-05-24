62cc0bb5058b89ceb36461b90f35ead26dfe12bc
3A <524019351@qq.com>
2020-05-24 19:46:35 +0800

    改层重名和训练BUG

--- 被压榨的人写的备胎.py
+++ 被压榨的人写的备胎.py
@@ -1,4 +1,3 @@
-import network_2
 import numpy as np
 import pandas as pd
 from keras import callbacks
@@ -8,8 +7,15 @@ import keras.optimizers as opt
 from keras import Input,layers
 from keras.models import Model
 import matplotlib.pyplot as plt
+
 df=pd.read_csv("infection_A.csv",names=["城市","区域","日期","增加人数"])
 df=df.drop(columns=["城市","日期"])
+
+
+def generate_arrays_from_file(x,y):
+    while True:
+        yield (x,y)
+
 for i in range(0,1):
     callback_list = [
         callbacks.EarlyStopping(monitor="loss", patience=500),
@@ -30,7 +36,6 @@ for i in range(0,1):
     model__1.summary()
     model__1.trainable=False
 
-
     data_input = Input(shape=(1,))
     x = layers.normalization.BatchNormalization()(data_input)
     x = layers.Dense(32, activation="relu")(x)
@@ -42,25 +47,20 @@ for i in range(0,1):
     y = layers.add([x, y])
     y = layers.Dense(128, activation="relu")(y)
     y = layers.Dense(1)(y)
-    predict_3 = layers.normalization.BatchNormalization()(y)
-    model__2 = Model(inputs=data_input, outputs=predict_3)
+    predict_2 = layers.normalization.BatchNormalization()(y)
 
+    middle = model__1(predict_2)
 
-
-    data_input = Input(shape=(1,))
-    z= layers.Dense(256,activation="relu")(data_input)
+    z = layers.Dense(256,activation="relu")(middle)
     predict_3 = layers.Dense(1)(z)
-    model__3 = Model(data_input, predict_3)
-    model__3.summary()
 
-    ensemble_input=keras.Input(shape=(1,))
-    ensemble_output=model__3(model__1(model__2(ensemble_input)))
-    ensemble_model=Model(ensemble_input,ensemble_output)
+    ensemble_model=Model(data_input,predict_3)
     ensemble_model.summary()
     print(train_data)
     print(target)
     ensemble_model.compile(optimizer=opt.adam(), loss="mse")
-    ensemble_model.fit(train_data,target, epochs=40000, batch_size=45, callbacks=callback_list)
+    ensemble_model.fit_generator(generate_arrays_from_file(train_data,target), steps_per_epoch=1, epochs=40000, callbacks=callback_list)
+
     y = ensemble_model.predict(train_data)
     y = y.reshape((45,))
     print(((y - target) ** 2).mean())
